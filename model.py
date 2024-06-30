16cc629da743b9a73fea93af5c2f49862eeb4388
kwkCrash <104627678+kwkCrash@users.noreply.github.com>
2024-07-01 01:40:40 +0800

    Create model.py

---
+++ model.py
@@ -0,0 +1,143 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SE(nn.Module):
+    def __init__(self, channels, reduction=16):
+        print(channels)
+        super(SE, self).__init__()
+        self.squeeze = nn.AdaptiveAvgPool2d((1, 1)) 
+        self.excitation = nn.Sequential(
+            nn.Conv2d(channels, channels // reduction, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channels // reduction, channels, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.Sigmoid())
+
+
+    def forward(self, x): 
+        b, c, _, _ = x.size()
+        y = self.squeeze(x)
+        y = self.excitation(y)
+        return x * y 
+
+
+
+class Segmentation_Model(nn.Module):
+
+    def __init__(self, embedding_dim = 160):
+        super(Segmentation_Model, self).__init__()
+
+        # Backbone
+        if model_type == 'B0':
+            self.backbone = mit.mit_b0()
+        if model_type == 'B1':
+            self.backbone = mit.mit_b1()
+        if model_type == 'B2':
+            self.backbone = mit.mit_b2()
+        if model_type == 'B3':
+            self.backbone = mit.mit_b3()
+        if model_type == 'B4':
+            self.backbone = mit.mit_b4()
+        if model_type == 'B5':
+            self.backbone = mit.mit_b5()
+
+        self._init_weights()
+
+        # LP Header
+        self.LP_1 = mlp.LP(input_dim = self.backbone.embed_dims[0], embed_dim = self.backbone.embed_dims[0])
+        self.LP_2 = mlp.LP(input_dim = self.backbone.embed_dims[1], embed_dim = self.backbone.embed_dims[1])
+        self.LP_3 = mlp.LP(input_dim = self.backbone.embed_dims[2], embed_dim = self.backbone.embed_dims[2])
+        self.LP_4 = mlp.LP(input_dim = self.backbone.embed_dims[3], embed_dim = self.backbone.embed_dims[3])
+
+        # Linear Fuse
+        self.linear_fuse34 = ConvModule(in_channels=(self.backbone.embed_dims[2] + self.backbone.embed_dims[3]), out_channels=self.backbone.embed_dims[2], kernel_size=1,norm_cfg=dict(type='BN', requires_grad=True))         
+        self.linear_fuse23 = ConvModule(in_channels=(self.backbone.embed_dims[1] + self.backbone.embed_dims[2]), out_channels=self.backbone.embed_dims[1], kernel_size=1,norm_cfg=dict(type='BN', requires_grad=True))
+        self.linear_fuse12 = ConvModule(in_channels=(self.backbone.embed_dims[0] + self.backbone.embed_dims[1]), out_channels=self.backbone.embed_dims[0], kernel_size=1,norm_cfg=dict(type='BN', requires_grad=True))
+
+        # Fused LP Header
+        self.LP_12 = mlp.LP(input_dim = self.backbone.embed_dims[0], embed_dim = self.backbone.embed_dims[0])
+        self.LP_23 = mlp.LP(input_dim = self.backbone.embed_dims[1], embed_dim = self.backbone.embed_dims[1])
+        self.LP_34 = mlp.LP(input_dim = self.backbone.embed_dims[2], embed_dim = self.backbone.embed_dims[2])
+
+        # Final Linear Prediction
+        self.linear_pred = nn.Conv2d((self.backbone.embed_dims[0] + self.backbone.embed_dims[1] + self.backbone.embed_dims[2] + self.backbone.embed_dims[3]), 2, kernel_size=1)
+
+        self.SE = SE(1024)
+
+    def _init_weights(self):
+        if model_type == 'B0':
+            pretrained_dict = torch.load('./Pretrained/mit_b0.pth')
+        if model_type == 'B1':
+            pretrained_dict = torch.load('./Pretrained/mit_b1.pth')
+        if model_type == 'B2':
+            pretrained_dict = torch.load('./Pretrained/mit_b2.pth')
+        if model_type == 'B3':
+            pretrained_dict = torch.load('./Pretrained/mit_b3.pth')
+        if model_type == 'B4':
+            pretrained_dict = torch.load('./Pretrained/mit_b4.pth')
+        if model_type == 'B5':
+            pretrained_dict = torch.load('./Pretrained/mit_b5.pth')
+
+
+        model_dict = self.backbone.state_dict()
+        pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
+        model_dict.update(pretrained_dict)
+        self.backbone.load_state_dict(model_dict)
+
+    def forward(self, x):
+        B = x.shape[0]
+
+        #stage 1
+        out_1, H, W = self.backbone.patch_embed1(x)
+        for i, blk in enumerate(self.backbone.block1):
+            out_1 = blk(out_1, H, W)
+        out_1 = self.backbone.norm1(out_1)
+        out_1 = out_1.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()
+        
+        #stage 2
+        out_2, H, W = self.backbone.patch_embed2(out_1)
+        for i, blk in enumerate(self.backbone.block2):
+            out_2 = blk(out_2, H, W)
+        out_2 = self.backbone.norm2(out_2)
+        out_2 = out_2.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()
+
+        # stage 3
+        out_3, H, W = self.backbone.patch_embed3(out_2)
+        for i, blk in enumerate(self.backbone.block3):
+            out_3 = blk(out_3, H, W)
+        out_3 = self.backbone.norm3(out_3)
+        out_3 = out_3.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()
+
+        # stage 4
+        out_4, H, W = self.backbone.patch_embed4(out_3)
+        for i, blk in enumerate(self.backbone.block4):
+            out_4 = blk(out_4, H, W)
+        out_4 = self.backbone.norm4(out_4)
+        out_4 = out_4.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()
+
+        # go through LP Header
+        lp_1 = self.LP_1(out_1)
+        lp_2 = self.LP_2(out_2)
+        lp_3 = self.LP_3(out_3)
+        lp_4 = self.LP_4(out_4)
+
+        # linear fuse and go pass LP Header
+        lp_34 = self.LP_34(self.linear_fuse34(torch.cat([lp_3, F.interpolate(lp_4,scale_factor=2,mode='bilinear', align_corners=False)], dim=1)))
+        lp_23 = self.LP_23(self.linear_fuse23(torch.cat([lp_2, F.interpolate(lp_34,scale_factor=2,mode='bilinear', align_corners=False)], dim=1)))
+        lp_12 = self.LP_12(self.linear_fuse12(torch.cat([lp_1, F.interpolate(lp_23,scale_factor=2,mode='bilinear', align_corners=False)], dim=1)))
+
+        # get the final output
+        lp4_resized = F.interpolate(lp_4,scale_factor=8,mode='bilinear', align_corners=False)
+        lp3_resized = F.interpolate(lp_34,scale_factor=4,mode='bilinear', align_corners=False)
+        lp2_resized = F.interpolate(lp_23,scale_factor=2,mode='bilinear', align_corners=False)
+        lp1_resized = lp_12
+
+        out = torch.cat([lp1_resized, lp2_resized, lp3_resized, lp4_resized], dim=1)
+        out = self.SE(out)
+        out = self.linear_pred(out)
+
+        out_resized = F.interpolate(out,scale_factor=4,mode='bilinear', align_corners=True)
+
+        return out_resized
+
+                                                                                 
